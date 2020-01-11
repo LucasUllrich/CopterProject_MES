6d6326ca6d6fca8bc31b0eeedb2b7ea038566182
Lucas Ullrich <es19m014@technikum-wien.at>
2020-01-11 18:21:52 +0100

    Remove src dir

--- hello_EK_TM4C1294XL_TI/main.c
+++ hello_EK_TM4C1294XL_TI/main.c
@@ -165,7 +165,7 @@ Int main()
     controlParams.arg0 = (UArg) (&uartMailbox);
     controlParams.arg1 = 0;
     controlParams.priority = 8;
-    controlParams.stackSize = 4096;
+//    controlParams.stackSize = 4096;
 
     controlTask = Task_create((Task_FuncPtr)controlPoller, &controlParams, &eb);
     if (controlTask == NULL)
